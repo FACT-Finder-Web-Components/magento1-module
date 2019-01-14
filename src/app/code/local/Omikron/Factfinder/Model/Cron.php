791a0a946d266cebdf642790f2a6af3953bdfdfb
a-laurowski <Panpiczek1!>
2019-01-14 09:57:02 +0100

    Update Changelog

--- src/app/code/local/Omikron/Factfinder/Model/Cron.php
+++ src/app/code/local/Omikron/Factfinder/Model/Cron.php
@@ -16,12 +16,18 @@ class Omikron_Factfinder_Model_Cron
     protected $configHelper;
 
     /**
+     * @var Mage_Core_Model_App_Emulation
+     */
+    protected $appEmulation;
+
+    /**
      * Omikron_Factfinder_Model_Cron constructor.
      */
     public function __construct()
     {
         $this->productExportModel = Mage::getModel('factfinder/export_product');
         $this->configHelper = Mage::helper('factfinder/data');
+        $this->appEmulation = Mage::getSingleton('core/app_emulation');
     }
 
     /**
@@ -30,8 +36,12 @@ class Omikron_Factfinder_Model_Cron
     public function exportFeed(Mage_Cron_Model_Schedule $schedule)
     {
         if ($this->configHelper->isCronEnabled()) {
+
             foreach (Mage::app()->getStores() as $store) {
+                $initialEnvironmentInfo = $this->appEmulation->startEnvironmentEmulation($store->getStoreId(), Mage_Core_Model_App_Area::AREA_ADMINHTML);
+                Mage::app()->addEventArea(Mage_Core_Model_App_Area::AREA_ADMINHTML);
                 $this->productExportModel->exportProduct($store);
+                $this->appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
             }
         }
     }
