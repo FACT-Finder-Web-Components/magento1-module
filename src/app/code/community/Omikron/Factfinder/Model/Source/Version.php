87d62b3df12595ebb52f8bc2cb7dbf8dee57c7a4
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-09-27 17:04:56 +0200

    Code review

--- src/app/code/community/Omikron/Factfinder/Model/Source/Version.php
+++ src/app/code/community/Omikron/Factfinder/Model/Source/Version.php
@@ -1,16 +1,15 @@
 <?php
 
-class Omikron_Factfinder_Model_Source_FFVersion
+class Omikron_Factfinder_Model_Source_Version
 {
     /**
-     * Options getter     *
      * @return array
      */
     public function toOptionArray()
     {
         return [
             ['value' => '7.2', 'label' => '7.2'],
-            ['value' => '7.3', 'label' => '7.3']
+            ['value' => '7.3', 'label' => '7.3'],
         ];
     }
 }
