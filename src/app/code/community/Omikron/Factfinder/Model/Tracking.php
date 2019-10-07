8965f8c6c49dcac32256648536243393c9867608
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-10-07 09:53:50 +0200

    Code Review

--- src/app/code/community/Omikron/Factfinder/Model/Tracking.php
+++ src/app/code/community/Omikron/Factfinder/Model/Tracking.php
@@ -18,32 +18,36 @@ class Omikron_Factfinder_Model_Tracking
 
     public function __construct()
     {
-        $this->communication    = Mage::helper('factfinder/communication');
-        $this->config           = Mage::helper('factfinder');
-        $this->sessionData      = Mage::getModel('factfinder/sessionData');
+        $this->communication = Mage::helper('factfinder/communication');
+        $this->config        = Mage::helper('factfinder');
+        $this->sessionData   = Mage::getModel('factfinder/sessionData');
     }
 
     /**
-     * @param string                     $event
+     * @param string            $event
      * @param TrackingProduct[] $trackingProducts
      */
     public function execute($event, array $trackingProducts)
     {
-        $params = [
-            'event'    => $event,
-            'channel'  => $this->config->getChannel(),
-            'products' => array_map(function (TrackingProduct $trackingProduct) {
-                return array_filter([
-                    'id'       => $trackingProduct->getTrackingNumber(),
-                    'masterId' => $trackingProduct->getMasterArticleNumber(),
-                    'price'    => $trackingProduct->getPrice(),
-                    'count'    => $trackingProduct->getCount(),
-                    'sid'      => $this->sessionData->getSessionId(),
-                    'userId'   => $this->sessionData->getUserId(),
-                ]);
-            }, $trackingProducts),
-        ];
+        try {
+            $params = [
+                'event'    => $event,
+                'channel'  => $this->config->getChannel(),
+                'products' => array_map(function (TrackingProduct $trackingProduct) {
+                    return array_filter([
+                        'id'       => $trackingProduct->getTrackingNumber(),
+                        'masterId' => $trackingProduct->getMasterArticleNumber(),
+                        'price'    => $trackingProduct->getPrice(),
+                        'count'    => $trackingProduct->getCount(),
+                        'sid'      => $this->sessionData->getSessionId(),
+                        'userId'   => $this->sessionData->getUserId(),
+                    ]);
+                }, $trackingProducts),
+            ];
 
-        $this->communication->sendToFF($this->apiName, $params);
+            $this->communication->sendToFF($this->apiName, $params);
+        } catch (Zend_Exception $e) {
+            Mage::logException($e);
+        }
     }
 }
