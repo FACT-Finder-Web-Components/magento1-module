ec1764b94d2c6152825d5db3a7bf062aa55ef968
Pavel Novikov <paulfcdd@gmail.com>
2021-07-20 11:44:52 +0200

    add dependencies

--- src/lib/CommunicationSDK/vendor/composer/autoload_real.php
+++ src/lib/CommunicationSDK/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit6d6c57f37d10cc895d7eb62fa20c003d
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit6d6c57f37d10cc895d7eb62fa20c003d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit6d6c57f37d10cc895d7eb62fa20c003d', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit6d6c57f37d10cc895d7eb62fa20c003d
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire6d6c57f37d10cc895d7eb62fa20c003d($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire6d6c57f37d10cc895d7eb62fa20c003d($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- src/lib/CommunicationSDK/vendor/composer/autoload_static.php
+++ src/lib/CommunicationSDK/vendor/composer/autoload_static.php
@@ -6,6 +6,55 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d
 {
+    public static $files = array (
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Omikron\\FactFinder\\Communication\\Client\\Client' => __DIR__ . '/../..' . '/src/Client/Client.php',
@@ -32,6 +81,8 @@ class ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit6d6c57f37d10cc895d7eb62fa20c003d::$classMap;
 
         }, null, ClassLoader::class);
